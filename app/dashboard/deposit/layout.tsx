1415d73061d3f0f3c6f9c8dfca7d3f5b5f35023b
KingsleyEffiong <kingsleyeffiong642@gmail.com>
2025-08-10 08:08:33 +0100

    updated the dashboard

---
+++ app/dashboard/deposit/layout.tsx
@@ -0,0 +1,10 @@
+// app/dashboard/layout.tsx
+import React, { Suspense } from "react";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<div>Loading dashboard...</div>}>
+      {children}
+    </Suspense>
+  );
+}
